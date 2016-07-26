20a9a114f9e59736f34ef3363c2f0d8774c64594
Benjamin Holman Richards IV <ben@gigroom.com>
2016-07-26 14:11:51 -0700

    Add vendor, add grunt tasks, vendor scripts loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,83 @@
 //Gruntfile
 module.exports = function(grunt) {
 
-  // requirejs compile options
-  var compileOptions = {
+  // Add global variables for asset locations
 
-      mainConfigFile: 'app/scripts/main.js',
-      baseUrl: 'app/scripts',
-      include: ['main'],
-      out: 'dist/main.min.js',
-      removeCombined: false,
-      findNestedDependencies: true,
 
-      //Removes console.logs for production
-      onBuildWrite: function (moduleName, path, contents) {
-          if(/(.*)js\/modules\/(.*)/.test(path)) return contents.replace(/console.log(.*);/g, ';');
-          return contents;
-      }
-  }
 
   //Initializing the configuration object
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     // Add global variables for asset locations
     dirs: {            
+        bower_folder: 'bower_components',
+        npm_folder: 'node_modules',
+
         sass_folder: 'app/sass',
         css_folder: 'app/css',
-        js_folder: './app/scripts',
-        images_folder: './app/images',
+        js_folder: 'app/js',
+        images_folder: 'app/images',
         // files for deploying
         production_build_folder: './app/production_build'
         // Usage Example: 
             // dest: '<%= dirs.sass_folder %>/assets/sass' 
     },
+    // Concat JS into single production file
+    concat: {
+        ui_vendor: {
+            src: [
+                '<%= dirs.bower_folder %>/jquery/dist/jquery.js',
+                '<%= dirs.bower_folder %>/underscore/underscore.js',
+                '<%= dirs.bower_folder %>/backbone/backbone.js',
+                '<%= dirs.bower_folder %>/marionette/lib/backbone.marionette.js',
+                '<%= dirs.bower_folder %>/bootstrap/dist/js/bootstrap.js',
+            ],
+            dest: '<%= dirs.js_folder %>/build/concat-ui_vendor.js',
+        },
+        // global_custom: {
+        //     src: [
+        //         '<%= dirs.js_folder %>/useful.js',  // This specific file
+        //         '<%= dirs.js_folder %>/local-storage.js',  // This specific file
+        //     ],
+        //     dest: '<%= dirs.js_folder %>/build/concat-global_custom.js',
+        // },
+    },
+    // Uglify JS
+    uglify: {
+        build: {
+            src: '<%= dirs.js_folder %>/build/concat-ui_vendor.js',
+            // src: '<%= dirs.js_folder %>/build/concat-dashboard_custom.js',
+            dest: '<%= dirs.production_build_folder %>/all-uglified.js'
+        }
+    },
     // Task configuration
     
-    less: {
-        development: {
-            options: {
-              compress: false,  // no minification in dev
-            },
-            files: {
-              //compiling base.less into styles.css
-              "./app/styles/styles.css":"./app/styles/base.less"
-            }
-        },
-        production: {
-          options: {
-            cleancss: true, // minify css
-            // compress: true, // minify css
-          },
-          files: {
-            //compiling base.less into main.min.css
-            "./dist/main.min.css": "./app/styles/base.less"
-          }
-        }
-    },
-    requirejs: {
-        compile: {
-            options : compileOptions
-        }
-    },
+    // less: {
+    //     development: {
+    //         options: {
+    //           compress: false,  // no minification in dev
+    //         },
+    //         files: {
+    //           //compiling base.less into styles.css
+    //           "./app/styles/styles.css":"./app/styles/base.less"
+    //         }
+    //     },
+    //     production: {
+    //       options: {
+    //         cleancss: true, // minify css
+    //         // compress: true, // minify css
+    //       },
+    //       files: {
+    //         //compiling base.less into main.min.css
+    //         "./dist/main.min.css": "./app/styles/base.less"
+    //       }
+    //     }
+    // },
+    // requirejs: {
+    //     compile: {
+    //         options : compileOptions
+    //     }
+    // },
 
     sass: {
         dist: {
@@ -112,37 +130,41 @@ module.exports = function(grunt) {
             livereload: true  
           }
         },
-        less: {
-            // Watch all .less files from the styles directory)
-            files: ['app/styles/*.less'],
-            tasks: ['less'],
-            // Reloads the browser
-            options: {
-              livereload: true  
-            }
-        },
-        requirejs: {
-            // Watch only main.js so that we do not constantly recompile the .js files
-            files: [ 'app/scripts/main.js' ],
-            tasks: [ 'requirejs' ],
-            // Reloads the browser
-            options: {
-              livereload: true  
-            }
-        }
+        // less: {
+        //     // Watch all .less files from the styles directory)
+        //     files: ['app/styles/*.less'],
+        //     tasks: ['less'],
+        //     // Reloads the browser
+        //     options: {
+        //       livereload: true  
+        //     }
+        // },
+        // requirejs: {
+        //     // Watch only main.js so that we do not constantly recompile the .js files
+        //     files: [ 'app/scripts/main.js' ],
+        //     tasks: [ 'requirejs' ],
+        //     // Reloads the browser
+        //     options: {
+        //       livereload: true  
+        //     }
+        // }
     }
   });
 
   // Plugin loading
   grunt.loadNpmTasks('grunt-contrib-sass'); 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  // grunt.loadNpmTasks('grunt-contrib-less');
+  // grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   
 
   // Task definition
-  grunt.registerTask('default', ['watch' ]);
+  grunt.registerTask('default', ['watch','concat','uglify' ]);
   grunt.registerTask('dev', ['sass' ]);
+  // grunt.registerTask('default', ['concat','uglify','compass:dev' ]);
 
 };
