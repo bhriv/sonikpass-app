bb567eda7b1699a289463aa7da5bc08caa8711b1
Benjamin Holman Richards IV <ben@gigroom.com>
2016-09-06 12:50:10 -0700

    initial cleanup

--- app/scripts/views/growth.js
+++ app/scripts/views/growth.js
@@ -5,8 +5,8 @@ define(['jquery','underscore','chartjs'],
 // inject into DOM
   // $('#faq_list').append(myFaqsView.el);
 
-  var headline = '<h1 class="headline-big">Growth</h1>';
-  var copy =      '<p class="copy">Sonikpass growth charts.</p>';
+  var headline = '<h1 class="headline-big">Finance</h1>';
+  var copy =      '<p class="copy">finance charts.</p>';
   var chart =     '<canvas id="growth_chart" width="400" height="400" style="margin:20px;"></canvas>';
   var chart_financial_summary =     '<canvas id="chart_financial_summary" width="400" height="400" style="margin:20px;"></canvas>';
   
--- app/scripts/views/growth_data.js
+++ app/scripts/views/growth_data.js
@@ -1,6 +1,31 @@
 //Filename: views/header.js
 define(['jquery','underscore','chartjs'], 
   function($, _,chartjs) {
+  	/* Margin Graph 
+    ---------------------------
+            2017  2018  2019  /
+    Sales   27    36    63    /
+    Margins 55    43    80    /
+    Profits 50    70    90    /
+    ---------------------------
+    */
+
+    var three_years = ["2017", "2018", "2019"];
+    var sales = [
+        "27",
+        "36",
+        "63",
+    ];
+    var margins = [
+        "55",
+        "43",
+        "80",
+    ];
+    var profits = [
+        "50",
+        "70",
+        "90",
+    ];
   	console.log('views/growth_data loaded');
 // inject into DOM
   // $('#faq_list').append(myFaqsView.el);
